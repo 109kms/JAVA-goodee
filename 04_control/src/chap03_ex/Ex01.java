e023b4216d3194f810ab03774984e5caaffc6c5c
109kms <109kms@naver.com>
2025-06-27 17:50:27 +0900

    250627

--- 04_control/src/chap03_ex/Ex01.java
+++ 04_control/src/chap03_ex/Ex01.java
@@ -6,6 +6,17 @@ public class Ex01 {
     
     // 정수형 변수 a와 b에 임의의 값을 저장한 뒤, 두 수의 합과 차를 출력하세요.
     // 차는 항상 양수 값으로 출력하세요.
+    int a = 5;
+    int b = 15;
+    int sum = a + b;
+    int minusResult = 0;
+    if (a < b) {
+      minusResult = b - a;
+    } else {
+      minusResult = a - b;
+    }
+    System.out.println(sum);
+    System.out.println(minusResult);
     
     
 
--- 04_control/src/chap03_ex/Ex02.java
+++ 04_control/src/chap03_ex/Ex02.java
@@ -5,7 +5,19 @@ public class Ex02 {
   public static void main(String[] args) {
     
     // 정수형 변수 3개에 임의의 값을 저장한 뒤, 그 중에서 가장 큰 수를 출력하세요.
+    int a = 15;
+    int b = 1;
+    int c = 9;
+    int max;
     
+    if (a > b && a > c) {
+      max = a;
+    } else if (b > a && b > c) {
+      max = b;
+    } else {
+      max = c;
+    }
+    System.out.println(max);
     
 
   }
