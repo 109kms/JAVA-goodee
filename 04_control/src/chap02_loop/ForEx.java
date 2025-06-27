0f94c3ff62875e8ec63cf5ad09d37892d04812c0
109kms <109kms@naver.com>
2025-06-27 12:44:30 +0900

    250627

--- 04_control/src/chap02_loop/ForEx.java
+++ 04_control/src/chap02_loop/ForEx.java
@@ -3,8 +3,24 @@ package chap02_loop;
 public class ForEx {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    
+    // 1 ~ 100 누적하기
+    int total = 0;
+    for (int x = 1; x <= 100; x++) {
+      total += x;
+    }
+    System.out.println(total);
 
+    // 1 ~ 100 홀수의 평균(합계 / 개수)
+    int oddTotal = 0;
+    int oddCount = 0;
+    double oddAverage;
+    for(int odd = 1; odd <= 100; odd += 2) {
+      oddTotal += odd;
+      oddCount += 1;
+    }
+    oddAverage = (double)oddTotal / oddCount; // oddTotal을 double로 casting하면, oddCount는 double로 promotion됩니다.
+    System.out.println(oddAverage);
   }
 
 }
