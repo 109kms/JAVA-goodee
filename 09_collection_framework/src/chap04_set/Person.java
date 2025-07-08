cec9a89d42b93e13052aa2269a6629449e3a3cd2
109kms <109kms@naver.com>
2025-07-08 15:46:28 +0900

    250708

--- 09_collection_framework/src/chap04_set/Person.java
+++ 09_collection_framework/src/chap04_set/Person.java
@@ -36,6 +36,7 @@ public class Person {
   
   //----- HashSet 객체에 Person 객체를 저장할 때 오버라이드된 hashCode()와 equals()를 이용하여
   //      기존에 저장된 Person 객체와 새로 저장하려는 Person 객체의 중복 여부를 판단합니다.
+  //      먼저 hashCode() 메소드로 해시값을 비교하고, 동일한 해시값을 가지고 있으면 equals() 메소드를 호출합니다.
 
   @Override
   public int hashCode() {
